16d88d8ad6f8d2c770024521171ae4b6086dea07
Shota Akhalaia <akhalaiashota@outlook.com>
2025-01-26 17:36:46 +0400

    Edit subnet v1.1

--- Azure/subnet/v1.1/outputs.tf
+++ Azure/subnet/v1.1/outputs.tf
@@ -1,9 +1,7 @@
 output "subnet_id" {
-  value = azurerm_subnet.example.id
-  description = "The ID of the created subnet"
+    value = azurerm_subnet.example[count.index].id  // Accessing the id using count.index
 }
 
 output "subnet_cidr" {
-  value = azurerm_subnet.example.address_prefixes[0]
-  description = "The CIDR of the first address prefix of the subnet"
+    value = azurerm_subnet.example[count.index].address_prefixes[0]  // Accessing the address_prefixes using count.index
 }
