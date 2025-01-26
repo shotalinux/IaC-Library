7ffba66a1aeb8062c74a1cd7f7b4d14b671b8e1b
Shota Akhalaia <akhalaiashota@outlook.com>
2025-01-26 17:29:45 +0400

    Edit subnet v1.1

--- Azure/subnet/v1.1/main.tf
+++ Azure/subnet/v1.1/main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_subnet" "example" {
-  count                 = length(var.address_prefixes)  # Automatically count based on the number of address prefixes
-  name                  = format("%s-%d", var.subnet_name, count.index)  // Simplified name formatting
+  count                 = length(var.address_prefixes)
+  name                  = format("%s-%d", var.subnet_name, count.index)
   resource_group_name   = var.rg_name
   virtual_network_name  = var.virtual_network_name
-  address_prefixes      = var.address_prefixes[count.index]
+  address_prefixes      = [var.address_prefixes[count.index]] # Wrap in a list
 
   dynamic "delegation" {
     for_each = var.delegations
