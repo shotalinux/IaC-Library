3152ee5fbaaf4f15c09dbe70e41588210c7a3b30
Azure DevOps <akhalaiashota@outlook.com>
2025-06-24 10:28:30 +0000

    Merge Azure DevOps master into feature-branch with squashed commit

--- contabo/instance/v1.0/main.tf
+++ contabo/instance/v1.0/main.tf
@@ -2,6 +2,7 @@ data "contabo_image" "debian_11" {
   id = "66abf39a-ba8b-425e-a385-8eb347ceac10"
 }
 
-resource "contabo_instance" "default_instance" {
-  image_id = data.contabo_image.debian_11.id
+resource "contabo_instance" "custom_instance" {
+  display_name  = "Debian 11 instance"
+  image_id      = data.contabo_image.debian_11.id
 }
--- contabo/instance/v1.0/outputs.tf
+++ contabo/instance/v1.0/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
   description = "ID of the created instance"
-  value       = contabo_instance.default_instance.id
+  value       = contabo_instance.custom_instance.id
 }
 
-output "instance_ip" {
-  description = "Public IP of the instance"
-  value       = contabo_instance.default_instance.public_ip
+output "custom_instance_output" {
+  description = "Our first custom instance"
+  value = contabo_instance.custom_instance
 }
